15eca5f0e817f2fa281b9d4dcb75f51ff014e013
Remi Collet <remi@remirepo.net>
2019-02-11 09:47:19 +0100

    Math/BigInteger is no more used

--- test/Horde/Mapi/Autoload.php
+++ test/Horde/Mapi/Autoload.php
@@ -8,8 +8,3 @@
  * @author     Jan Schneider <jan@horde.org>
  * @license    http://www.horde.org/licenses/lgpl21 LGPL 2.1
  */
-
-/* Load Math_BigInteger now, before composer autoloaders can mess with the
- * include path. This is to avoid using phpseclib's Math\BigInteger from
- * Horde_Pgp instead of PEAR's Math_BigInteger. */
-require_once 'Math/BigInteger.php';
